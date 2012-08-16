d9e2b7210f86925b751ece58c4cc0ec2d2a32736
Moxiaoyong <1987mxy@gmail.com>
2012-08-17 00:21:46 +0800

    debug Securer and record note
    fixed bug about display error when unpack secure key, must open 'Key'
    file by binary.
    Secure logic of Secvice Center are organized and recording

--- ServiceCenter/lib/Securer/Securer.py
+++ ServiceCenter/lib/Securer/Securer.py
@@ -23,20 +23,18 @@ class Securer(object):
 		self.__now = int( time.time() )
 		
 		keyFile = open('./Key','rb')
-		#time 5byte
-		#keyString unit 4byte 
 		packTime = keyFile.read(8)
 		keyTime = unpack( '<Q', packTime )
-		print self.__now - keyTime[0]
+
 		keySeek = ( ( ( self.__now - keyTime[0] ) / 60 ) - 1 ) * 4
-		print keySeek
 		keyFile.seek( keySeek + 8 )
 		keyBin = keyFile.read( 8 )
-		print keyBin.__repr__()
 		keyFile.close()
-		keyList = unpack( '<LL', keyBin )
+		
+		keyNum = len( keyBin ) / 4
+		keyList = unpack( '<'+'L'*keyNum , keyBin )
 		return keyList
-	
+		
 	def check(self, key):
 		if not self.switch: return True
 		return ( int(key) in self.__read() ) 
